eeeb14c36a87e602be33c38636dfe7cb0a363ad3
Syedzada999 <94744396+Syedzada999@users.noreply.github.com>
2021-11-21 14:13:21 +0500

    Create Hacker.py
    apt update
    apt upgrade
    Pkg install git
    Pkg install git 
    Pkg install python
    Pip install requests
    Pip install bs4
    git clone https://github.com/Syedzada999/hacker.git
    Cd burteFB
    ls
    Hacker.py

---
+++ Hacker.py
@@ -0,0 +1,26 @@
+# BruteFb
+
+HACK FACEBOOK DENGAN METODE BRUTEFORCE DI TERMUX
+
+# Cara Install
+
+### $ pkg update && pkg upgrade
+
+## $ pkg install git
+
+## $ pkg install python
+
+## $ pip install requests
+
+## $ pip install bs4
+
+## $ git clone https://github.com/SYEDHacker/BruteFb
+
+## $ cd BruteFb
+
+## $ python fb.py
+
+## $ usernam : syed12
+
+## $ password :123456
+
